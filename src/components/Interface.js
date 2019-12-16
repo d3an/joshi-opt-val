4ec4f7aef67210f1eb354e8c4110bfa6e5756d43
d3an <jabury@sympatico.ca>
2019-12-16 00:27:37 -0500

    Added developer's notes and future improvements. Some html edits.

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -28,15 +28,15 @@ export default class OptionValuation extends Component {
 
     axios.get(`https://montecarlo.monti.workers.dev/?type=${this.state.type}&dte=${this.state.daysToExp}&strike=${this.state.strike}&rf=${this.state.riskFreeRate}&spot=${this.state.spot}&vol=${this.state.volatility}&steps=${this.state.steps}`)
       .then(res => {
-        console.log(res);
-        console.log(res.data);
+        // console.log(res);
+        // console.log(res.data);
         this.setState({'payoff': res.data});
       })
       .catch(err => {
-        console.log(err)
-        console.log("Potentially exceeded capacity limits...")
-        console.log("Request with step count > 10000 is not consistent.")
-        console.log("Try again with step count <= 10000")
+        // console.log(err)
+        // console.log("Potentially exceeded capacity limits...")
+        // console.log("Request with step count > 10000 is not consistent.")
+        // console.log("Try again with step count <= 10000")
       });
   }
 
@@ -114,7 +114,17 @@ export default class OptionValuation extends Component {
             </Row>
             <Row className="fullcredits">
               <Col xs={12} md={12}>
-                <p>Recently I've been reading Mark Joshi's book titled C++ Design Patterns &amp; Derivatives and </p>
+                <h3>Developer's Note</h3>
+                <p>This is a serverless app that uses C++, React, JavaScript, and WebAssembly to calculate the payoffs of vanilla financial derivatives.</p>
+                <p>The frontend of this app was built solely by myself during a 6 hour period on Sunday December 15, 2019. Development time was between 1pm and 7pm, mainly during the Coffee and Code meetup taking place at The Tempered Press in the Trinity Bellwoods neighbourhood of Toronto.</p>
+                <p>As an attempt to break into the quant developer field, I've been studying option valuations over the last few months. My favourite technical book on option valuation has been C++ Design Patterns &amp; Derivatives Pricing by Mark Joshi. The majority of the C++ codebase for this app can be found in Chapter 2 of his book.</p>
+                <p>The rest of this app's backend was built by myself as I followed the practices found in Level Up with WebAssembly by Robert Aboukhalil. Prior to reading this book, I was stumbling around in the dark trying to refactor every WebAssembly project or tutorial I could find. Unfortunately, most development with this new technology is done with Rust and very basic web architecture. Very little is done with C++, much less with React.</p>
+                <p>The app's backend is currently running as a serverless function deployed on Cloudflare's Worker platform. The Cloudflare endpoint has been benchmarked to have its slowest run at 50ms, which is at least 10x faster than AWS Lambdas, Google Cloud Functions, and Azure Functions at their fastest runtimes.</p>
+                <h3>Coming Next</h3>
+                <p>Future improvements may include a switch to Fastly's Compute@Edge platform, once it becomes live for commercial use. Fastly's platform features a 35µs function initialization time, which would be a very impressive speed-up for this use case.</p>
+                <p>I'll also continue to update the Cloudflare worker as I progress through more of Joshi's book. My plan is to add support for more complex derivatives, as well as a choice of the randomization method used. Currently the app uses the Box-Muller randomization algorithm implemented by Mark Joshi.</p>
+                <p>With regards to the app's reliability, I plan to add a WebAssembly loader switch that allows a user to specify how they'd like to compute their request. Currently, Cloudflare times out at 50ms of computation time. In other words, if the step count is greater than 100,000, then there is a high chance the function request will fail. By adding a WebAssembly loader switch, users could take responsibility for their own computations by running the function on their personal CPUs.</p>
+                <p>I've struggled heavily trying to add this functionality to an app developed with create-react-app. The best way to solve this problem would be to eject create-react-app. However, I'd prefer to avoid working directly with Webpack for as long as I can.</p>
               </Col>
             </Row>
           </Col>
